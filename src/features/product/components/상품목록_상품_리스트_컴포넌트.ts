637b5c9070879e407d59e2ea01c3ee369fe050af
진돌 <yeojinseok@kakao.com>
2025-07-09 23:52:18 +0900

    refactor: 상품 리스트 컴포넌트 클래스 방식으로 리팩토링

---
+++ src/features/product/components/상품목록_상품_리스트_컴포넌트.ts
@@ -0,0 +1,24 @@
+import { Component } from "../../../../componet";
+import { ProductListResponse } from "../../../api/productApi";
+import { productStore } from "../store/product-store";
+import { 상품_아이템_컴포넌트 } from "./상품_아이템_컴포넌트";
+
+export class 상품목록_상품_리스트_컴포넌트 extends Component {
+  render(): HTMLElement {
+    const productList = productStore.value.data;
+
+    const products = productList.flatMap((v) => v.products);
+    const el = document.createElement("div");
+    el.innerHTML = /* HTML */ `
+      <div>
+        <div class="mb-4 text-sm text-gray-600">
+          총 <span class="font-medium text-gray-900">${products.length}개</span>의 상품
+        </div>
+        <div class="grid grid-cols-2 gap-4 mb-6" id="products-grid">
+          ${products.map((product) => 상품_아이템_컴포넌트(product)).join("")}
+        </div>
+      </div>
+    `;
+    return el;
+  }
+}
