f1cb5b9b90240f2d022333b8db2a79ad857a31c7
진돌 <wlstjrghdud@naver.com>
2025-07-10 00:21:22 +0900

    feat: IntersectionObserver 모의 구현 추가

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,6 +3,25 @@ import { configure } from "@testing-library/dom";
 import { afterAll, beforeAll } from "vitest";
 import { server } from "./__tests__/mockServerHandler.js";
 
+// Mock IntersectionObserver for Vitest/JSDOM
+defineIntersectionObserverMock();
+
+function defineIntersectionObserverMock() {
+  if (typeof window !== "undefined" && !window.IntersectionObserver) {
+    class IntersectionObserver {
+      constructor() {}
+      observe() {}
+      unobserve() {}
+      disconnect() {}
+      takeRecords() {
+        return [];
+      }
+    }
+    window.IntersectionObserver = IntersectionObserver;
+    window.IntersectionObserverEntry = function () {};
+  }
+}
+
 configure({
   asyncUtilTimeout: 5000,
 });
