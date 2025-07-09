670cec7837164e8a137a61cbb9b44a2cbd505fa4
진돌 <yeojinseok@kakao.com>
2025-07-09 23:51:19 +0900

    feat: component 추상화 클래스 추가

---
+++ componet.ts
@@ -0,0 +1,53 @@
+export abstract class Component<P extends object = {}> {
+  public test = false;
+  public isMounted = false;
+
+  el: HTMLElement;
+
+  parentEl?: HTMLElement;
+
+  protected _props: P;
+
+  constructor(props?: P) {
+    this._props = props ?? ({} as P);
+    this.el = this.render();
+  }
+
+  get props(): P {
+    return this._props;
+  }
+
+  abstract render(): HTMLElement;
+
+  update() {
+    const newEl = this.render();
+    this.el.replaceWith(newEl);
+    this.el = newEl;
+    this.onUpdate();
+  }
+
+  mount(target: HTMLElement) {
+    if (this.isMounted) {
+      return;
+    }
+    this.parentEl = target;
+    this.parentEl.appendChild(this.el);
+    this.isMounted = true;
+    this.componentDidMount();
+  }
+
+  unmount() {
+    if (!this.isMounted) {
+      return;
+    }
+
+    this.componentWillUnmount();
+    this.parentEl?.removeChild(this.el);
+    this.isMounted = false;
+  }
+
+  onUpdate() {}
+
+  componentDidMount() {}
+  componentWillUnmount() {}
+}
